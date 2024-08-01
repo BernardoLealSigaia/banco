03bb603c5c0d869715c43f91ac893d2ccb3f27ed
Bernardo Leal Sigaia <bernardo.l.sigaia@gmail.com>
2024-08-01 17:30:16 -0300

    BD - Ordenando pela letra inical do nome

--- consulta.php
+++ consulta.php
@@ -6,7 +6,24 @@
 
     if (mysqli_num_rows($resultado) > 0){
         while($registro = mysqli_fetch_assoc($resultado)) {
-        echo "id: " . $registro["id"]. " - Nome" . $registro["nome"].
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
+        " " . $registro["senha"]. " - Email:" . $registro["email"].
+        "<br>";
+        }
+    } else{
+        echo "Nenhum registro encontrado.";
+    }
+
+
+    //segunda consulta
+    echo "<br>Nomes que começam com 'c' <br>";
+    $sql = "SELECT id, nome, senha, email FROM usuario where nome like 'b%'";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+
+    if (mysqli_num_rows($resultado) > 0){
+        while($registro = mysqli_fetch_assoc($resultado)) {
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
         " " . $registro["senha"]. " - Email:" . $registro["email"].
         "<br>";
         }
