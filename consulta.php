944120cf714c198dd13cda8e01d826655223d061
Bernardo Leal Sigaia <bernardo.l.sigaia@gmail.com>
2024-08-01 17:37:34 -0300

    Ordenando por email

--- consulta.php
+++ consulta.php
@@ -1,5 +1,8 @@
 <?php
     include "conecta.php";
+    echo "<br><br>";
+
+    echo "<b>Mostrando todos os registros</b><br>";
     $sql = "SELECT id, nome, senha, email FROM usuario";
     $resultado = mysqli_query($conexao, $sql);
     echo "<br>";
@@ -16,7 +19,7 @@
 
 
     //segunda consulta
-    echo "<br>Nomes que começam com 'c' <br>";
+    echo "<br><b>Nomes que começam com 'c' </b><br>";
     $sql = "SELECT id, nome, senha, email FROM usuario where nome like 'b%'";
     $resultado = mysqli_query($conexao, $sql);
     echo "<br>";
@@ -30,4 +33,22 @@
     } else{
         echo "Nenhum registro encontrado.";
     }
+
+    //terceira consulta
+    echo "<br><b>Ordenar registros por email</b><br>";
+    $sql = "SELECT id, nome, senha, email FROM usuario order by email";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+
+    if (mysqli_num_rows($resultado) > 0){
+        while($registro = mysqli_fetch_assoc($resultado)) {
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
+        " " . $registro["senha"]. " - Email:" . $registro["email"].
+        "<br>";
+        }
+    } else{
+        echo "Nenhum registro encontrado.";
+    }
+
+    
 ?>
